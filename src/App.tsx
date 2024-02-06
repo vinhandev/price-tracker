a39d73f247b375c0581d44520ef3fb2774592d2e
vinhandev <vinhan.dev@gmail.com>
2024-02-06 08:12:11 +0700

    update name

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,101 @@ type Props = {
 function App() {
   const initData: Props[] = [
     {
-      name: 'CLICKBUY: iPhone 13 128GB Cũ đẹp 99% 128Gb',
+      name: 'APPLE: iPhone 13 128GB',
       date: new Date(),
-      link: 'https://clickbuy.com.vn/apple-iphone-13-128gb-chinh-hang-cu.html',
+      link: 'https://www.apple.com/vn/shop/buy-iphone/iphone-13',
       price: '0',
-      first: `<span>Black <br> <span class="font-normal">`,
+      first: `<spanclass="dimensionCapacity">128<small>GB<spanclass="visuallyhidden">Chúthích</span>²</small></span><spanclass="dimensionColor">Hồng</span><spanclass="carrier-logos">Unlocked</span><spanclass="price"><spanclass="current_price">`,
+      last: `<`,
+      color: '#FF8911',
+    },
+    {
+      name: 'FPT: iPhone 13 128GB | Chính hãng VN/A',
+      date: new Date(),
+      link: 'https://fptshop.com.vn/dien-thoai/iphone-13',
+      price: '0',
+      first: `iPhone13128GB</a></span><p>`,
+      last: `<`,
+      color: '#CDFADB',
+    },
+    {
+      name: 'THEGIOIDIDONG: iPhone 13 128GB | Chính hãng VN/A',
+      date: new Date(),
+      link: 'https://www.thegioididong.com/dtdd/iphone-13',
+      price: '0',
+      first: `<p class="box-price-present">`,
+      last: `&#x2`,
+      color: '#CDFADB',
+    },
+    {
+      name: 'CELLPHONES: iPhone 13 128GB | Chính hãng VN/A',
+      date: new Date(),
+      link: 'https://cellphones.com.vn/iphone-13.html',
+      price: '0',
+      first: `<div data-v-da80e888><strong>128GB</strong></div><span data-v-da80e888>`,
+      last: `</span>`,
+      color: '#CDFADB',
+    },
+    {
+      name: 'HOANGHAMOBILE: Điện thoại iPhone 13 (128GB) - Chính hãng VN/A',
+      date: new Date(),
+      link: 'https://hoanghamobile.com/dien-thoai-di-dong/apple-iphone-13-128gb-chinh-hang-vn-a',
+      price: '0',
+      first: `<span><label><strong>128GB</strong></label></span><strong>`,
+      last: `</strong>`,
+      color: '#CDFADB',
+    },
+    {
+      name: 'SHOPDUNK: Điện thoại iPhone 13 (128GB) - Chính hãng VN/A',
+      date: new Date(),
+      link: 'https://shopdunk.com/iphone-13',
+      price: '0',
+      first: `<span id="price-value-59" class="new-price price-value-59">`,
+      last: `</span>`,
+      color: '#CDFADB',
+    },
+    {
+      name: 'VIETTEL: iPhone 13 128GB',
+      date: new Date(),
+      link: 'https://viettelstore.vn/dien-thoai/iphone-13-128gb-pid288660.html',
+      price: '0',
+      first: `<div class="version-product active">iPhone 13 128GB<span class="txt-price" data-id="288660">`,
+      last: `<span`,
+      color: '#CDFADB',
+    },
+    {
+      name: '2TMOBILE: iPhone 13 128GB Chính hãng',
+      date: new Date(),
+      link: 'https://2tmobile.com/iphone-13-128gb-chinh-hang-vn-a/',
+      price: '0',
+      first: `<span class="woocommerce-Price-amount amount"><bdi>`,
+      last: `&nbsp`,
+      color: '#CDFADB',
+    },
+    {
+      name: 'DIENMAYCHOLON: iPhone 13 128GB Chính Hãng (VN/A)',
+      date: new Date(),
+      link: 'https://dienmaycholon.vn/dien-thoai-di-dong/iphone-13-128gb-chinh-hang-vna',
+      price: '0',
+      first: `<span class="price-pro">`,
+      last: `</span>`,
+      color: '#CDFADB',
+    },
+    {
+      name: 'PHUCKHANGMOBILE: iPhone 13 128GB Quốc Tế (Zin - 99%)',
+      date: new Date(),
+      link: 'https://phuckhangmobile.com/iphone-13-128gb-quoc-te-zin-99-6085.html',
+      price: '0',
+      first: `<span class="value">Zin - 99%</span><span class="price">`,
       last: `</span>`,
       color: '#EBD9B4',
     },
     {
-      name: 'CLICKBUY: iPhone 13 Lock 128GB Cũ đẹp 99%',
+      name: 'XTMOBILE: iPhone 13 128GB (Cũ 99%)',
       date: new Date(),
-      link: 'https://clickbuy.com.vn/iphone-13-128gb-lock-cu-dep-99.html',
+      link: 'https://www.xtmobile.vn/iphone-13-128gb-cu-likenew',
       price: '0',
-      first: `<span>Black <br> <span class="font-normal">`,
+      first: `<span class="price" id="price" itemprop="price" content="11999000">`,
       last: `</span>`,
       color: '#EBD9B4',
     },
@@ -40,13 +121,40 @@ function App() {
       color: '#EBD9B4',
     },
     {
-      name: '2TMOBILE: iPhone 13 128GB Chính hãng',
+      name: 'DIDONGVIET: iPhone 13 128GB (Likenew)',
       date: new Date(),
-      link: 'https://2tmobile.com/iphone-13-128gb-chinh-hang-vn-a/',
+      link: 'https://didongviet.vn/may-cu-gia-re/iphone-13-128gb-likenew.html',
       price: '0',
-      first: `<span class="woocommerce-Price-amount amount"><bdi>`,
-      last: `&nbsp`,
+      first: `{"@type":"Offer","price":"`,
+      last: `"`,
       color: '#EBD9B4',
+    },
+    {
+      name: 'PHUCKHANGMOBILE: iPhone 13 128GB Quốc Tế (Likenew - 98%)',
+      date: new Date(),
+      link: 'https://phuckhangmobile.com/iphone-13-128gb-quoc-te-likenew-98-6102.html',
+      price: '0',
+      first: `<span class="value">Likenew - 98%</span><span class="price">`,
+      last: `</span>`,
+      color: '#EBD9B4',
+    },
+    {
+      name: 'CLICKBUY: iPhone 13 128GB Cũ đẹp 99%',
+      date: new Date(),
+      link: 'https://clickbuy.com.vn/apple-iphone-13-128gb-chinh-hang-cu.html',
+      price: '0',
+      first: `<p class="price">`,
+      last: `<`,
+      color: '#EBD9B4',
+    },
+    {
+      name: 'CLICKBUY: iPhone 13 Lock 128GB Cũ đẹp 99%',
+      date: new Date(),
+      link: 'https://clickbuy.com.vn/iphone-13-128gb-lock-cu-dep-99.html',
+      price: '0',
+      first: `<p class="price">`,
+      last: `<`,
+      color: '#E78895',
     },
     {
       name: 'MOBILECITY: Điện thoại Xiaomi Redmi Note 12 Turbo (Snapdragon 7+ Gen 2) 8-128gb',
@@ -120,6 +228,15 @@ function App() {
       last: `&nbsp`,
       color: '#CDFADB',
     },
+    {
+      name: 'TRUNGTRAN.VN: Macbook Pro 14 M1 Pro RAM 16GB SSD 512GB Retina 2021',
+      date: new Date(),
+      link: 'https://trungtran.vn/macbook-pro-14-m1-pro-2021/',
+      price: '0',
+      first: `<span class="_price">`,
+      last: `<`,
+      color: '#CDFADB',
+    },
   ];
   const [count, setCount] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
@@ -138,11 +255,13 @@ function App() {
           //   'Access-Control-Allow-Methods':'PUT, GET, HEAD, POST, DELETE, OPTIONS'
           // },
         });
-        setProgressValue((progressValue) => progressValue + 1);
+        setProgressValue(index + 1);
         const data = (await response.text())
           .replace(/\n/g, ' ')
           .replace(/\r/g, ' ')
           .replace(/\t/g, ' ')
+          .split('=""')
+          .join('')
           .split(' ')
           .join('');
         console.log(index, data);
