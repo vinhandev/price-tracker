30dd3865d4e99739038f664d845be09ec2a31eb3
vinhandev <vinhan.dev@gmail.com>
2024-03-29 18:26:23 +0700

    clean code

--- src/components/atoms/Buttons/Button/Button.tsx
+++ src/components/atoms/Buttons/Button/Button.tsx
@@ -1,8 +1,6 @@
 import { ButtonProps, Button as RButton } from '@mui/material';
-import { useColors } from '@/hooks';
 
 export default function Button(props: ButtonProps) {
-  const colors = useColors();
   return (
     <RButton
       {...props}
