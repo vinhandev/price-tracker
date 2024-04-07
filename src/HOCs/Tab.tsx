897092c19f49d25d76e30c145f40ade5632680fc
vinhandev <vinhan.dev@gmail.com>
2024-04-07 19:10:55 +0700

    update skeleton for other page

--- src/HOCs/Tab.tsx
+++ src/HOCs/Tab.tsx
@@ -1,5 +1,6 @@
 import { useColors } from '@/hooks';
 import { Card, Typography } from '@mui/material';
+import { SkeletonWrapper } from '.';
 
 type Props = {
   title?: string;
@@ -9,35 +10,37 @@ type Props = {
 export default function Tab({ title, children, noPadding = false }: Props) {
   const colors = useColors();
   return (
-    <Card
-      elevation={0}
-      sx={{
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        background: colors.background,
-        borderRadius: 3,
-        padding: noPadding ? 0 : '20px',
-        transition: 'background 1s ease',
+    <SkeletonWrapper>
+      <Card
+        elevation={0}
+        sx={{
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column',
+          background: colors.background,
+          borderRadius: 3,
+          padding: noPadding ? 0 : '20px',
+          transition: 'background 1s ease',
 
-        width: '100%',
-        height: '100%',
-      }}
-    >
-      {title ? (
-        <Typography
-          sx={{
-            paddingTop: noPadding ? '20px' : 0,
-            paddingX: noPadding ? '20px' : 0,
-            fontFamily: 'Roboto',
-            fontWeight: '700',
-            color: colors.text,
-          }}
-        >
-          {title}
-        </Typography>
-      ) : null}
-      {children}
-    </Card>
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {title ? (
+          <Typography
+            sx={{
+              paddingTop: noPadding ? '20px' : 0,
+              paddingX: noPadding ? '20px' : 0,
+              fontFamily: 'Roboto',
+              fontWeight: '700',
+              color: colors.text,
+            }}
+          >
+            {title}
+          </Typography>
+        ) : null}
+        {children}
+      </Card>
+    </SkeletonWrapper>
   );
 }
--- src/screens/Main/Products.tsx
+++ src/screens/Main/Products.tsx
@@ -21,7 +21,7 @@ import NavigationIcon from '@mui/icons-material/Navigation';
 import { useEffect, useMemo, useState } from 'react';
 import { useColors } from '@/hooks';
 import { useStore, useUser } from '@/store';
-import { Tab } from '@/HOCs';
+import { SkeletonWrapper, Tab } from '@/HOCs';
 import { showSuccess, updateFirebasePrices } from '@/utils';
 import AddIcon from '@mui/icons-material/Add';
 import { useNavigate } from 'react-router-dom';
@@ -113,204 +113,206 @@ export default function ProductsScreen() {
   }, []);
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        overflow: 'hidden',
-        paddingTop: '10px',
-      }}
-    >
-      <Dialog
+    <SkeletonWrapper>
+      <Box
         sx={{
-          '.MuiPaper-root': {
-            width: 500,
-            padding: '10px',
-          },
+          display: 'flex',
+          flexDirection: 'column',
+          overflow: 'hidden',
+          paddingTop: '10px',
         }}
-        open={open}
-        onClose={handleClose}
       >
-        <DialogTitle>New Product</DialogTitle>
-        <DialogContent>
-          <TextInput
-            onChange={setNewProductName}
-            value={newProductName}
-            label="Name"
-            errorText=""
-            isError={false}
-          />
-        </DialogContent>
-        <Divider />
-        <DialogActions>
-          <Button onClick={handleAddNewProduct}>Add</Button>
-          <Button color="inherit" onClick={handleClose}>
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <Tab>
-        <Box>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: '10px',
-            }}
-          >
-            <Typography
-              variant="h5"
+        <Dialog
+          sx={{
+            '.MuiPaper-root': {
+              width: 500,
+              padding: '10px',
+            },
+          }}
+          open={open}
+          onClose={handleClose}
+        >
+          <DialogTitle>New Product</DialogTitle>
+          <DialogContent>
+            <TextInput
+              onChange={setNewProductName}
+              value={newProductName}
+              label="Name"
+              errorText=""
+              isError={false}
+            />
+          </DialogContent>
+          <Divider />
+          <DialogActions>
+            <Button onClick={handleAddNewProduct}>Add</Button>
+            <Button color="inherit" onClick={handleClose}>
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Tab>
+          <Box>
+            <Box
               sx={{
-                color: colors.text,
+                display: 'flex',
+                alignItems: 'center',
+                gap: '10px',
               }}
             >
-              Products
-            </Typography>
-            <IconButton
-              onClick={handleClose}
-              sx={{
-                borderRadius: 1000,
-                background: colors.primary,
-                padding: '2px',
-                transition: 'background 0.3s ease',
-                ':hover': {
-                  background: `${colors.primary}DD`,
-                },
-              }}
-            >
-              <AddIcon
+              <Typography
+                variant="h5"
                 sx={{
-                  width: '18px',
-                  height: '18px',
-                  color: colors.text2,
+                  color: colors.text,
                 }}
-              />
-            </IconButton>
-          </Box>
-          {priceList.length > 0 ? (
-            <TableContainer
-              sx={{
-                paddingTop: '10px',
-                flex: 1,
-                background: colors.background,
-                transition: 'background 1s ease',
-                color: colors.text,
-              }}
-            >
-              <Table
-                stickyHeader
-                sx={{
-                  '.MuiTableCell-root': {
-                    color: colors.text3,
-                    background: colors.background,
-                    transition: 'background 1s ease',
-                    paddingY: '7px',
-                    fontFamily: 'Roboto',
-                    fontWeight: '400',
-                    fontSize: '14px',
-                    ':hover': {
-                      color: colors.primary,
-                    },
-                  },
-                  '.MuiTableRow-root': {
-                    background: colors.background,
-                    transition: 'background 1s ease',
-                    ':hover': {
-                      backgroundColor: 'rgba(0, 0, 0, 0.1)',
-                      cursor: 'pointer',
-                    },
-                  },
-                  '.MuiTableCell-head': {
-                    fontWeight: '300',
-                    color: 'grey',
-                  },
-                }}
-                aria-label="simple table"
               >
-                <TableHead>
-                  <TableRow>
-                    <TableCell
-                      sx={{
-                        paddingLeft: 0,
-                      }}
-                    >
-                      NAME
-                    </TableCell>
-                    <TableCell>SHOPS' QUANTITY</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {priceList.map((item) => (
-                    <TableRow
-                      key={item.label}
-                      onClick={() => {
-                        setSelectedProduct(item.label);
-                        navigate(
-                          getPath({
-                            path: 'PRODUCT',
-                            params: {
-                              productId: convertLabelToUrl(item.label),
-                            },
-                          })
-                        );
-                      }}
-                    >
+                Products
+              </Typography>
+              <IconButton
+                onClick={handleClose}
+                sx={{
+                  borderRadius: 1000,
+                  background: colors.primary,
+                  padding: '2px',
+                  transition: 'background 0.3s ease',
+                  ':hover': {
+                    background: `${colors.primary}DD`,
+                  },
+                }}
+              >
+                <AddIcon
+                  sx={{
+                    width: '18px',
+                    height: '18px',
+                    color: colors.text2,
+                  }}
+                />
+              </IconButton>
+            </Box>
+            {priceList.length > 0 ? (
+              <TableContainer
+                sx={{
+                  paddingTop: '10px',
+                  flex: 1,
+                  background: colors.background,
+                  transition: 'background 1s ease',
+                  color: colors.text,
+                }}
+              >
+                <Table
+                  stickyHeader
+                  sx={{
+                    '.MuiTableCell-root': {
+                      color: colors.text3,
+                      background: colors.background,
+                      transition: 'background 1s ease',
+                      paddingY: '7px',
+                      fontFamily: 'Roboto',
+                      fontWeight: '400',
+                      fontSize: '14px',
+                      ':hover': {
+                        color: colors.primary,
+                      },
+                    },
+                    '.MuiTableRow-root': {
+                      background: colors.background,
+                      transition: 'background 1s ease',
+                      ':hover': {
+                        backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                        cursor: 'pointer',
+                      },
+                    },
+                    '.MuiTableCell-head': {
+                      fontWeight: '300',
+                      color: 'grey',
+                    },
+                  }}
+                  aria-label="simple table"
+                >
+                  <TableHead>
+                    <TableRow>
                       <TableCell
                         sx={{
                           paddingLeft: 0,
                         }}
                       >
-                        {item.label}
+                        NAME
                       </TableCell>
-                      <TableCell>{item.numOfWebsites}</TableCell>
+                      <TableCell>SHOPS' QUANTITY</TableCell>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          ) : (
-            <Box>
-              <Divider />
-              <Typography
-                sx={{
-                  paddingTop: 2,
-                  color: colors.text,
-                  fontSize: '12px',
-                  fontWeight: '300',
-                  fontFamily: 'Roboto',
-                  textAlign: 'center',
-                }}
-              >
-                No data.
-              </Typography>
-            </Box>
-          )}
-        </Box>
-      </Tab>
-      <Zoom
-        in={isActive}
-        timeout={transitionDuration}
-        style={{
-          transitionDelay: `${isActive ? transitionDuration.exit : 0}ms`,
-        }}
-        unmountOnExit
-      >
-        <Fab
-          sx={{
-            padding: '10px',
-            position: 'fixed',
-            bottom: '30px',
-            right: '30px',
+                  </TableHead>
+                  <TableBody>
+                    {priceList.map((item) => (
+                      <TableRow
+                        key={item.label}
+                        onClick={() => {
+                          setSelectedProduct(item.label);
+                          navigate(
+                            getPath({
+                              path: 'PRODUCT',
+                              params: {
+                                productId: convertLabelToUrl(item.label),
+                              },
+                            })
+                          );
+                        }}
+                      >
+                        <TableCell
+                          sx={{
+                            paddingLeft: 0,
+                          }}
+                        >
+                          {item.label}
+                        </TableCell>
+                        <TableCell>{item.numOfWebsites}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            ) : (
+              <Box>
+                <Divider />
+                <Typography
+                  sx={{
+                    paddingTop: 2,
+                    color: colors.text,
+                    fontSize: '12px',
+                    fontWeight: '300',
+                    fontFamily: 'Roboto',
+                    textAlign: 'center',
+                  }}
+                >
+                  No data.
+                </Typography>
+              </Box>
+            )}
+          </Box>
+        </Tab>
+        <Zoom
+          in={isActive}
+          timeout={transitionDuration}
+          style={{
+            transitionDelay: `${isActive ? transitionDuration.exit : 0}ms`,
           }}
-          variant="extended"
-          size="medium"
-          color="primary"
-          onClick={handleScrollToTop}
+          unmountOnExit
         >
-          <NavigationIcon sx={{ mr: 1, fontSize: 18 }} />
-          Scroll to top
-        </Fab>
-      </Zoom>
-    </Box>
+          <Fab
+            sx={{
+              padding: '10px',
+              position: 'fixed',
+              bottom: '30px',
+              right: '30px',
+            }}
+            variant="extended"
+            size="medium"
+            color="primary"
+            onClick={handleScrollToTop}
+          >
+            <NavigationIcon sx={{ mr: 1, fontSize: 18 }} />
+            Scroll to top
+          </Fab>
+        </Zoom>
+      </Box>
+    </SkeletonWrapper>
   );
 }
